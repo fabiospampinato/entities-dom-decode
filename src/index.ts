93925ba30d08e68717b67cb953fb2e741af8d9e3
Fabio Spampinato <spampinabio@gmail.com>
2021-06-06 23:05:09 +0100

    Caching decoded entities

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@
 
 const re = /(&(?:[a-zA-Z0-9]+|#[0-9]{1,6}|#[xX][a-fA-F0-9]{1,6});)/g;
 const span = document.createElement ( 'span' );
+const cache: Record<string, string> = {};
 
 /* MAIN */
 
 const decode = ( str: string ): string => {
 
   return str.replace ( re, ( match: string, entity: string ) => {
+    if ( entity in cache ) return cache[entity];
     span.innerHTML = entity;
-    return span.textContent || entity;
+    return cache[entity] = span.textContent || entity;
   });
 
 };
